dae88641046849a941ea073b4e2ab2097d55a679
Truongan <vutruongan15112003@gmail.com>
2024-12-19 14:05:52 +0700

    Update BTTH4

--- BTTH4/app/Models/Issue.php
+++ BTTH4/app/Models/Issue.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Issue extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'computer_id',
+        'reporter_by',
+        'reporter_date',
+        'description',
+        'urgency',
+        'status'
+    ];
+
+    public function computer()
+    {
+        return $this->belongsTo(Computer::class, 'computer_id', ownerKey: 'id');
+    }
 }
